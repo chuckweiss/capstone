879e19ad085b1c0d34429a40f8bc3221bbc1cbf1
Chuck <charlieweiss6@gmail.com>
2022-02-24 15:11:51 -0500

    button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,19 +3,46 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [data, setData] = React.useState(null);
+  // const [data, setData] = React.useState(null);
+
+  // React.useEffect(() => {
+  //   fetch("/api")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // }, []);
+
+  // <p>{!data ? "Loading..." : data}</p>
+
+  // <form action="/post" method="post" className="form">
+  //   <button type="submit">Connected?</button>
+  // </form>;
+
+  const [data, setData] = React.useState("data");
+
+  // <p>{!data ? "Loading..." : data}</p>
+
+  const data_update = (api) => {
+    fetch(api)
+      .then((res) => res.json())
+      .then((d) => {
+        setData(d.message);
+      });
+  };
+
+  // data_update("api2");
 
   React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+    document.title = `You clicked ${!data ? 0 : data} times`;
+  });
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+
+        <p>You clicked {!data ? 0 : data} times</p>
+
+        <button onClick={() => data_update("api")}>Click me</button>
       </header>
     </div>
   );
